43cc1e09755b5384e5c3d81e8ec5c9b04cef5abe
Chandrachur Mukherjee <chandrachur67@gmail.com>
2023-05-05 21:33:38 +0530

    implemented updating in db after a delay

--- App.jsx
+++ App.jsx
@@ -6,24 +6,51 @@ import { db, notesCollection } from "./firebase"
 import { onSnapshot, addDoc, setDoc, deleteDoc, doc } from "firebase/firestore"
 
 export default function App() {
-    const [notes, setNotes] = React.useState(
-        () => JSON.parse(localStorage.getItem("notes")) || []
-    )
+    const [notes, setNotes] = React.useState([])
     const [currentNoteId, setCurrentNoteId] = React.useState("")
+    const [tempNoteText, setTempNoteText] = React.useState("");
 
     const currentNote =
         notes.find(note => note.id === currentNoteId)
         || notes[0]
 
+    async function createNewNote() {
+        const newNote = {
+            body: "# Type your markdown note's title here",
+            createdAt: Date.now(),
+            updatedAt: Date.now()
+        }
+        const newNoteRef = await addDoc(notesCollection, newNote);
+        console.log(newNoteRef.id)
+        setCurrentNoteId(newNoteRef.id)
+    }
+
+    async function updateNote(text) {
+        const docRef = doc(db, "notes", currentNoteId);
+        await setDoc(docRef, { body: text, updatedAt: Date.now() }, { merge: true });
+    }
+
+    async function deleteNote(event, noteId) {
+        event.stopPropagation()
+        const docRef = doc(db, "notes", noteId)
+        await deleteDoc(docRef)
+    }
+
     React.useEffect(() => {
         const unSubscribe = onSnapshot(notesCollection, (snapShot) => {
-            console.log(snapShot.docs)
-            console.log(snapShot.docs[0].id)
-            const body = snapShot.docs[0].data()
-            console.log(body)
-            const newNotes = snapShot.docs.map(doc => {
-                return { ...doc.data(), id: doc.id }
-            })
+            // console.log(snapShot.docs)
+            let newNotes = [];
+            if (snapShot.docs.length > 0) {
+                // console.log(snapShot.docs[0].id)
+                // const body = snapShot.docs[0].data()
+                // console.log(body)
+                newNotes = snapShot.docs.map(doc => {
+                    return { ...doc.data(), id: doc.id }
+                })
+                newNotes.sort((note1, note2) => {
+                    return note2.updatedAt - note1.updatedAt
+                })
+            }
             setNotes(newNotes)
         })
         return unSubscribe
@@ -35,30 +62,20 @@ export default function App() {
         }
     }, [notes])
 
-    async function createNewNote() {
-        const newNote = {
-            body: "# Type your markdown note's title here"
+    React.useEffect(() => {
+        if (currentNote) {
+            setTempNoteText(currentNote?.body)
         }
-        const newNoteRef = addDoc(notesCollection, newNote);
-        console.log(newNoteRef)
-        setCurrentNoteId(newNoteRef)
-    }
+    }, [currentNote])
 
-    async function updateNote(text) {
-        const docRef = doc(db, "notes", currentNoteId);
-        const data = {
-            ...currentNote,
-            body: text
-        }
-
-        await setDoc(docRef, data);
-    }
-
-    async function deleteNote(event, noteId) {
-        event.stopPropagation()
-        const docRef = doc(db, "notes", noteId)
-        await deleteDoc(docRef)
-    }
+    React.useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            if (tempNoteText !== currentNote.body) {
+                updateNote(tempNoteText)
+            }
+        }, 500)
+        return () => clearTimeout(timeoutId)
+    }, [tempNoteText])
 
     return (
         <main>
@@ -81,8 +98,8 @@ export default function App() {
                             currentNoteId &&
                             notes.length > 0 &&
                             <Editor
-                                currentNote={currentNote}
-                                updateNote={updateNote}
+                                tempNoteText={tempNoteText}
+                                setTempNoteText={setTempNoteText}
                             />
                         }
                     </Split>
